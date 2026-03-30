ad93bbd82ce3e12f0eb412ce065a300f5ae67ba7
William Matton <williammatton@gmail.com>
2026-03-30 14:19:54 +0200

    Set up routes to the React-built frontend.

--- backend/node/app.js
+++ backend/node/app.js
@@ -1,11 +1,19 @@
 import express from 'express';
+import fs from 'fs/promises'
 
 export default function initializeApp() {
   const app = express();
 
-  app.get('/', (req, res) => {
-    res.send('Hello World!');
-  })
+  /*
+  API routes går via /api/resurs !
+  */
+
+  app.get('/', async(req, res) => {
+    const html = await fs.readFile('../frontend/dist/index.html');
+    res.send(html.toString());
+  });
+
+  app.use('/assets', express.static('../frontend/dist/assets'));
 
   return app;
 }
