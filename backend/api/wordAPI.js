76da5479b3b8cb88fa82e12bedd52ed37603d441
William Matton <williammatton@gmail.com>
2026-04-01 13:33:09 +0200

    Implement routing and backend algorithm for evaluating word guesses.

--- backend/api/wordAPI.js
+++ backend/api/wordAPI.js
@@ -14,8 +14,57 @@ async function getWord(length = 0, repeat = true) {
   return matches[Math.floor(Math.random() * matches.length)];
 }
 
+async function evaluateGuess(guessedWord, correctWord) {
+  const guess = guessedWord.trim().toLowerCase();
+  const answer = correctWord.trim().toLowerCase();
+
+  const areInputWordsInvalid =
+    guess.length !== answer.length ||
+    answer.length !== guess.length;
+  
+  if(areInputWordsInvalid)
+      return false;
+
+  const wordLength = answer.length;
+
+  let wordArray = [];
+  let frequencyMap = new Map();
+
+  if(guess === answer) {
+    for(let i = 0; i < wordLength; i++) {
+      wordArray[i] = {letter: guess[i], result: 'correct'};
+    }
+    return wordArray;
+  }
+
+  for(let i = 0; i < wordLength; i++) {
+    frequencyMap.set(answer[i], (frequencyMap.get(answer[i]) ?? 0) + 1);
+  }
+
+  for(let i = 0; i < wordLength; i++) {
+    if(guess[i] === answer[i]) {
+      wordArray[i] = {letter: guess[i], result: 'correct'};
+      frequencyMap.set(answer[i], frequencyMap.get(answer[i]) - 1);
+    }
+  }
+
+  for(let i = 0; i < wordLength; i++) {
+    if(wordArray[i] !== undefined) continue;
+    
+    if(answer.includes(guess[i]) && frequencyMap.get(guess[i]) > 0) {
+      wordArray[i] = {letter: guess[i], result: 'misplaced'};
+      frequencyMap.set(guess[i], frequencyMap.get(guess[i]) - 1);
+    }
+    else {
+      wordArray[i] = {letter: guess[i], result: 'incorrect'};
+    }
+  }
+  return wordArray;
+}
+
 const wordAPI = {
-  getWord
+  getWord,
+  evaluateGuess
 };
 
 export default wordAPI;
--- backend/node/app.js
+++ backend/node/app.js
@@ -16,6 +16,19 @@ export default function initializeApp() {
     res.status(200).send(word);
   });
 
+  app.get('/api/evaluateGuess', async(req, res) => {
+    const { guess, answer } = req.query;
+    if(!guess || !answer)
+      res.send(`Invalid call to /api/evaluateGuess, format is: /api/evaluateGuess?guess='yourGuess'&answer='yourAnswer'`);
+    else {
+      const evaluation = await wordAPI.evaluateGuess(guess, answer);
+      if(evaluation === false)
+        res.send('Invalid call to /api/evaluateGuess, the guessed word must be the same length as the answer.');
+      else
+        res.status(200).json({ evaluation: evaluation });
+    }
+  });
+
   app.use('/assets', express.static('../frontend/dist/assets'));
 
   return app;
